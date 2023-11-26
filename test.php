5aa0f0adf566d434368452b9da3ce9375418edf8
maikl-dzion <maikldzion1967@gmail.com>
2023-11-26 19:22:08 +0300

    wwwwwwwwwwwwwww
    wwwwwwwwwww

--- test.php
+++ test.php
@@ -4,46 +4,31 @@ require 'DatabaseInterface.php';
 require 'DatabaseClass.php';
 
 $config = [
-    'host'     => 'bolderp5.beget.tech',
-    'user'     => 'bolderp5_garden',
-    'passwd'   => '12QW34er',
+    'host' => 'bolderp5.beget.tech',
+    'user' => 'bolderp5_garden',
+    'passwd' => '12QW34er',
     'password' => '12QW34er',
-    'dbname'   => 'bolderp5_garden',
-    'driver'   => 'mysql',
-    'port'     => false,
+    'dbname' => 'bolderp5_garden',
+    'driver' => 'mysql',
+    'port' => false,
 ];
 
-
 $db = new DatabaseClass($config);
-
 
 $result = $param = $response = [];
 
 $sql = "SELECT * FROM `basicrefbook` LIMIT 5 ";
 
-//$sql = "SELECT * FROM `basicrefbook`
-//        WHERE `resourcetype` = :p2
-//        AND `pagename` = :p1";
-//
-//$param = [':p1' => 'basicrefbook',
-//          ':p2'    => 'video'];
-//
-//$sql = "SELECT * FROM `basicrefbook`
-//        WHERE `id` = :p1 ";
-//$param = [':p1' => 82];
-
 $response = $db->fetchData($sql, $param, false);
-
-// $response = $db->query($sql, 'select');
 
 dd($response);
 
 $data = [
-    'id'           => 82,
-    'name'         => 'Статья о финансовой грамотrttttt',
-    'page'         => 'basicrefbook',
+    'id' => 82,
+    'name' => 'Статья о финансовой грамотrttttt',
+    'page' => 'basicrefbook',
     'resourcetype' => 'article',
-    'article'      => 'Наши преподава6677тели трейдеры с многолетним стажем, rrrrtt
+    'article' => 'Наши преподава6677тели трейдеры с многолетним стажем, rrrrtt
                        которые благодаря своему опыту и знаниям создали 
                        эффективный теоретический курс для обучения успешной торговли vbbbbbb',
     'unique_material_status' => 'null',
@@ -51,15 +36,10 @@ $data = [
 
 $table = 'basicrefbook';
 
-// $response = $db->save($data, $table, 'insert');
-// $response = $db->save($data, $table, 'update', ['resourcetype' => 'video']);
-// $response = $db->save($data, $table, 'update');
-// $response = $db->save(['id' => 81], $table, 'delete');
-
 dd($response);
 
-
-function dd() {
+function dd()
+{
 
     $out = '';
     $get = false;
@@ -81,7 +61,7 @@ function dd() {
     $line = print_r($debugTrace, true);
     $out .= '<div style="' . $style . '" ><pre>' . $line . '</pre></div>';
 
-    if ($get) 
+    if ($get)
         return $out;
     print $out;
     exit;
