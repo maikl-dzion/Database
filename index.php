e0252a187af77d4162d9b2fb0ef0e5cf28681625
maikl-dzion <maikldzion1967@gmail.com>
2023-11-26 19:26:34 +0300

    dddddd
    ddddddd

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use App\Database;
 
-$config = ['host'     => '185.63.191.96',
-           'database' => 'faximile',
-           'user'     => 'w1user',
-           'password' => 'w1password',
-           'driver'   => 'pgsql',
-           'port'     => 5432,
-          ];
+$config = ['host' => '185.63.191.96',
+    'database' => 'faximile',
+    'user' => 'w1user',
+    'password' => 'w1password',
+    'driver' => 'pgsql',
+    'port' => 5432,
+];
 
 $pdo = Database::getInstance($config);
 
@@ -18,29 +18,20 @@ $table = 'desktop_list';
 
 $results = $pdo->select($table, [], '*', ' LIMIT 2')->result();
 
-$where = ['id = ?', [16] ];
-// $user    = $pdo->select($table, $where)->one();
-var_export($results); die;
+$where = ['id = ?', [16]];
+var_export($results);
 
-$data = array (
-    'title'   => 'Рабочий стол 8',
+$data = array(
+    'title' => 'Рабочий стол 8',
     'vidgets' => 'rrr',
-    'note'    => 'rrrr',
-    'name'    => 'rrr',
+    'note' => 'rrrr',
+    'name' => 'rrr',
 );
 
-//$r = $pdo->insert($table, $data);
-//var_dump($r);
-
-$data = array (
-    'name'    => 'ertyuuuuuu',
+$data = array(
+    'name' => 'ertyuuuuuu',
 );
-
 $where = ['id', 16];
-
 $r = $pdo->update($table, $data, $where);
 var_export($pdo);
-
-$where = ['id = ?', [17] ];
-//$r = $pdo->delete($table, $where);
-//var_export($r);
+$where = ['id = ?', [17]];
